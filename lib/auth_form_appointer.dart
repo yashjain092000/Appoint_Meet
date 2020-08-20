98c96f6c014aa9188599609f10ee5d2cf08c7b3c
yashjain092000 <yashjain092000@gmail.com>
2020-08-20 23:16:48 +0530

    create a stful widget authformappointer

--- lib/auth_form_appointer.dart
+++ lib/auth_form_appointer.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class AuthFormAppointer extends StatefulWidget {
+  @override
+  _AuthFormAppointerState createState() => _AuthFormAppointerState();
+}
+
+class _AuthFormAppointerState extends State<AuthFormAppointer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
