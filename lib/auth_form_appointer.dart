9f268cc85789d33a4d62edae8d59c3622efbb354
yashjain092000 <yashjain092000@gmail.com>
2020-08-20 23:22:45 +0530

    return cards containing text form fields with field validators for each field

--- lib/auth_form_appointer.dart
+++ lib/auth_form_appointer.dart
@@ -71,6 +71,218 @@ class _AuthFormAppointerState extends State<AuthFormAppointer> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    key: ValueKey('email'),
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email address',
+                    ),
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
+                  ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: 'Username'),
+                      onSaved: (value) {
+                        _userName = value;
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('phn'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter at least 4 characters';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: 'Phone'),
+                      onSaved: (value) {
+                        _phn = value;
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('address'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter a valid address';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: 'Address'),
+                      onSaved: (value) {
+                        _address = value;
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('specialisation'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Please enter a valid specialisation';
+                        }
+                        return null;
+                      },
+                      decoration:
+                          InputDecoration(labelText: 'Your Specialisation'),
+                      onSaved: (value) {
+                        _specialisation = value;
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('fee'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length > 4) {
+                          return 'Please enter a valid consultation fee';
+                        }
+                        return null;
+                      },
+                      decoration:
+                          InputDecoration(labelText: 'Consultation Fee'),
+                      onSaved: (value) {
+                        _fee = value;
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('emFee'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length > 4) {
+                          return 'Please enter a valid emergency fee';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: 'Emergency Fee'),
+                      onSaved: (value) {
+                        _emFee = value;
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('morTime1'),
+                      validator: (value) {
+                        if (value.isEmpty || int.parse(value) > 12) {
+                          return 'Please enter a valid time';
+                        }
+                        return null;
+                      },
+                      decoration:
+                          InputDecoration(labelText: 'Morning Starting time'),
+                      onSaved: (value) {
+                        _morTime1 = int.parse(value);
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('morTime2'),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter a valid time';
+                        }
+                        return null;
+                      },
+                      decoration:
+                          InputDecoration(labelText: 'Morning ending time'),
+                      onSaved: (value) {
+                        _morTime2 = int.parse(value);
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('eveTime1'),
+                      validator: (value) {
+                        if (value.isEmpty || int.parse(value) > 12) {
+                          return 'Please enter a valid time';
+                        }
+                        return null;
+                      },
+                      decoration:
+                          InputDecoration(labelText: 'Evening starting time'),
+                      onSaved: (value) {
+                        _eveTime1 = int.parse(value);
+                      },
+                    ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('eveTime2'),
+                      validator: (value) {
+                        if (value.isEmpty || int.parse(value) > 12) {
+                          return 'Please enter a valid time';
+                        }
+                        return null;
+                      },
+                      decoration:
+                          InputDecoration(labelText: 'Evening ending time'),
+                      onSaved: (value) {
+                        _eveTime2 = int.parse(value);
+                      },
+                    ),
+                  TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
+                  ),
+                  SizedBox(height: 12),
+                  if (widget.isLoading) CircularProgressIndicator(),
+                  if (!widget.isLoading)
+                    RaisedButton(
+                      child: Text(_isLogin ? 'Login' : 'Signup'),
+                      onPressed: _trySubmit,
+                    ),
+                  if (!widget.isLoading)
+                    FlatButton(
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text(_isLogin
+                          ? 'Create new account'
+                          : 'I already have an account'),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                    )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
