fde30a4c9b83af4ff060f85aa1de2d424eecb2f1
yashjain092000 <yashjain092000@gmail.com>
2020-08-20 22:59:19 +0530

    create a stful widget and class constructor

--- lib/auth_form_appointee.dart
+++ lib/auth_form_appointee.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class AuthFormAppointee extends StatefulWidget {
+  AuthFormAppointee(
+    this.submitFn,
+    this.isLoading,
+  );
+
+  @override
+  _AuthFormAppointeeState createState() => _AuthFormAppointeeState();
+}
+
+class _AuthFormAppointeeState extends State<AuthFormAppointee> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
