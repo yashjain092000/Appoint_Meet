ee065048ab49665ccb6ba4633b6954a448df38e5
yashjain092000 <yashjain092000@gmail.com>
2020-08-20 23:17:39 +0530

    declare required vars and constructor

--- lib/auth_form_appointer.dart
+++ lib/auth_form_appointer.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 
 class AuthFormAppointer extends StatefulWidget {
+  AuthFormAppointer(
+    this.submitFn,
+    this.isLoading,
+  );
+
+  final bool isLoading;
+
+  final void Function(
+    String email,
+    String password,
+    String userName,
+    String phn,
+    String address,
+    String specialisation,
+    String fee,
+    String emFee,
+    int morTime1,
+    int morTime2,
+    int eveTime1,
+    int eveTime2,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
   @override
   _AuthFormAppointerState createState() => _AuthFormAppointerState();
 }
