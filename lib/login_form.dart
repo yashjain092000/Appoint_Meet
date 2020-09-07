84ab60bfdbac2328dadd171ac9ebe769e9846354
yashjain092000 <yashjain092000@gmail.com>
2020-09-07 21:06:48 +0530

    add animated text in login form

--- lib/login_form.dart
+++ lib/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 
@@ -39,85 +40,100 @@ class _AuthFormState extends State<AuthForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        shadowColor: Colors.deepPurple,
-        elevation: 20.0,
-        margin: EdgeInsets.all(20),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextFormField(
-                    key: ValueKey('email'),
-                    validator: (value) {
-                      if (value.isEmpty || !value.contains('@')) {
-                        return 'Please enter a valid email address.';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepPurple),
+    return Column(
+      children: [
+        SizedBox(height: 60.0),
+        TypewriterAnimatedTextKit(
+            speed: Duration(milliseconds: 300),
+            totalRepeatCount: 0,
+            // repeatForever: false,
+            text: ["Welcome Back!"],
+            textStyle: TextStyle(
+                fontSize: 30, fontFamily: "Agne", fontWeight: FontWeight.w500),
+            textAlign: TextAlign.start,
+            alignment: AlignmentDirectional.topStart),
+        SizedBox(height: 60.0),
+        Center(
+          child: Card(
+            shadowColor: Colors.deepPurple,
+            elevation: 20.0,
+            margin: EdgeInsets.all(20),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(16),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      TextFormField(
+                        key: ValueKey('email'),
+                        validator: (value) {
+                          if (value.isEmpty || !value.contains('@')) {
+                            return 'Please enter a valid email address.';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepPurple),
+                            ),
+                            labelText: 'Email address',
+                            labelStyle: TextStyle(color: Colors.grey[600])),
+                        onSaved: (value) {
+                          _userEmail = value;
+                        },
+                      ),
+                      TextFormField(
+                        key: ValueKey('password'),
+                        validator: (value) {
+                          if (value.isEmpty || value.length < 7) {
+                            return 'Password must be at least 7 characters long.';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                            focusedBorder: UnderlineInputBorder(
+                              borderSide: BorderSide(color: Colors.deepPurple),
+                            ),
+                            labelText: 'Password',
+                            labelStyle: TextStyle(color: Colors.grey[600])),
+                        obscureText: true,
+                        onSaved: (value) {
+                          _userPassword = value;
+                        },
+                      ),
+                      SizedBox(height: 12),
+                      if (widget.isLoading) CircularProgressIndicator(),
+                      if (!widget.isLoading)
+                        RaisedButton(
+                          elevation: 6.0,
+                          color: Colors.deepPurple,
+                          textColor: Colors.white,
+                          child: Text('Login'),
+                          onPressed: _trySubmit,
                         ),
-                        labelText: 'Email address',
-                        labelStyle: TextStyle(color: Colors.grey[600])),
-                    onSaved: (value) {
-                      _userEmail = value;
-                    },
+                      if (!widget.isLoading)
+                        FlatButton(
+                          textColor: Colors.deepPurple,
+                          child: Text("Forgot Password?"),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => LoginScreen(),
+                                ));
+                          },
+                        )
+                    ],
                   ),
-                  TextFormField(
-                    key: ValueKey('password'),
-                    validator: (value) {
-                      if (value.isEmpty || value.length < 7) {
-                        return 'Password must be at least 7 characters long.';
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(
-                        focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.deepPurple),
-                        ),
-                        labelText: 'Password',
-                        labelStyle: TextStyle(color: Colors.grey[600])),
-                    obscureText: true,
-                    onSaved: (value) {
-                      _userPassword = value;
-                    },
-                  ),
-                  SizedBox(height: 12),
-                  if (widget.isLoading) CircularProgressIndicator(),
-                  if (!widget.isLoading)
-                    RaisedButton(
-                      elevation: 6.0,
-                      color: Colors.deepPurple,
-                      textColor: Colors.white,
-                      child: Text('Login'),
-                      onPressed: _trySubmit,
-                    ),
-                  if (!widget.isLoading)
-                    FlatButton(
-                      textColor: Colors.deepPurple,
-                      child: Text("Forgot Password?"),
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => LoginScreen(),
-                            ));
-                      },
-                    )
-                ],
+                ),
               ),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
